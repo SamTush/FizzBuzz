6bea725e8a8d02a92fd3affbeaa82dc6bbd5f0c3
SamTush <samo4421@icloud.com>
2023-02-01 16:43:01 +0300

    Clone fizzbuzz repo

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+  for (let i = 1; i <= 100; i++){
+    if (i%3===0){
+        console.log("Fizz");
+    }else if (i%5===0) {
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
+}
